a1c84ede0ecdbc095e3840916bea7fdc28781133
harshitha719 <55902125+harshitha719@users.noreply.github.com>
2019-11-30 08:23:11 -0600

    Add files via upload
    product bean updated to match product table attributes

--- Sample Rest Web Service/demo/src/main/java/com/example/beans/Product.java
+++ Sample Rest Web Service/demo/src/main/java/com/example/beans/Product.java
@@ -1,22 +1,126 @@
 package com.example.beans;
 
 public class Product {
-	
+	/*
+	 * productId
+	 */
 	private Integer productId;
-	
+	/*
+	 * productName
+	 */
 	private String productName;
-	
+	/*
+	 * productCategory
+	 */
 	private String productCategory;
-	
+	/*
+	 * productDescription
+	 */
 	private String productDescription;
-	
-	private String productBrand;
-	
-	private String productManufacturer;
-	
+	/*
+	 * productRating
+	 */
+	private Double productRating;
+	/*
+	 * productUnitSalePrice
+	 */
+	private Double productUnitSalePrice;
+	/*
+	 * productModel
+	 */
 	private String productModel;
-	
-	
-	
-
+	/*
+	 * @return productId
+	 */
+	public Integer getProductId() {
+		return productId;
+	}
+	/*
+	 * sets productId
+	 */
+	public void setProductId(Integer productId) {
+		this.productId = productId;
+	}
+	/**
+	 * 
+	 * @return productName
+	 */
+	public String getProductName() {
+		return productName;
+	}
+	/*
+	 * sets productId
+	 */
+	public void setProductName(String productName) {
+		this.productName = productName;
+	}
+	/**
+	 * 
+	 * @return productCategory
+	 */
+	public String getProductCategory() {
+		return productCategory;
+	}
+	/*
+	 * sets productId
+	 */
+	public void setProductCategory(String productCategory) {
+		this.productCategory = productCategory;
+	}
+	/**
+	 * 
+	 * @return productDescription
+	 */
+	public String getProductDescription() {
+		return productDescription;
+	}
+	/**
+	 * 
+	 * @param productDescription
+	 */
+	public void setProductDescription(String productDescription) {
+		this.productDescription = productDescription;
+	}
+	/**
+	 * 
+	 * @return productRating
+	 */
+	public Double getProductRating() {
+		return productRating;
+	}
+	/**
+	 * 
+	 * @param productBrand
+	 */
+	public void setProductRating(Double productBrand) {
+		this.productRating = productBrand;
+	}
+	/**
+	 * 
+	 * @return productUnitSalePrice
+	 */
+	public Double getProductUnitSalePrice() {
+		return productUnitSalePrice;
+	}
+	/**
+	 * 
+	 * @param productUnitSalePrice
+	 */
+	public void setProductUnitSalePrice(Double productUnitSalePrice) {
+		this.productUnitSalePrice = productUnitSalePrice;
+	}
+	/**
+	 * 
+	 * @return productModel
+	 */
+	public String getProductModel() {
+		return productModel;
+	}
+	/**
+	 * 
+	 * @param productModel
+	 */
+	public void setProductModel(String productModel) {
+		this.productModel = productModel;
+	}
 }
