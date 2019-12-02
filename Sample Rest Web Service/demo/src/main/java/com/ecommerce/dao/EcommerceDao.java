42031a2038640b41340885fbdd6876a2ac18013b
nishaaher17 <55922378+nishaaher17@users.noreply.github.com>
2019-12-01 22:44:03 -0600

    Update EcommerceDao.java
    added updateCheckoutTables method

--- Sample Rest Web Service/demo/src/main/java/com/ecommerce/dao/EcommerceDao.java
+++ Sample Rest Web Service/demo/src/main/java/com/ecommerce/dao/EcommerceDao.java
@@ -1,5 +1,6 @@
 package com.ecommerce.dao;
 
+import java.beans.Statement;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
@@ -44,5 +45,53 @@ public class EcommerceDao {
 		}
 		// code to load the result set in hash map
 		return productMap;
+	}
+	
+	public static void updateCheckoutTables(){
+		productMap = new HashMap<Integer,Product>();
+		//connection is fetched from connection provider
+		Connection con=DBConnection.getCon();
+		//query is done database
+		PreparedStatement ps;
+		try {
+
+			String CustomerID;
+			String EmailID;
+			String FullName;
+			String Address;
+			String City;
+			String State;
+			int Zip;
+			int MobNo;
+			int CreditCardNo;
+			String CreditCardType;
+			String ExpDate;
+			int CVV;
+
+			String sql = "INSERT INTO Customer VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+
+			ps = con.prepareStatement(sql);
+			//Add individual field values from array of values to be inserted to Preparedstatement
+			ps.setString(1, CustomerID);
+			ps.setString(2, EmailID);
+			ps.setString(3, FullName);
+			ps.setString(4, Address);
+			ps.setString(5, City);
+			ps.setString(6, State);
+			ps.setString(7, Zip);
+			ps.setString(8, MobNo);
+			ps.setString(9, CreditCardNo);
+			ps.setString(10, CreditCardType);
+			ps.setString(11, ExpDate);
+			ps.setString(12, CVV);
+			//Execute the insert query/prepated statement
+			ps.executeUpdate();
+
+			}
+		 catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		// code to load the result set in hash map
 	}
 }
