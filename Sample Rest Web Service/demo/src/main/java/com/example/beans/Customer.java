8ba3da3556c1b426f2dcdd2a6efdfe6dcda4f872
nishaaher17 <55922378+nishaaher17@users.noreply.github.com>
2019-12-01 22:45:21 -0600

    Create Customer.java

---
+++ Sample Rest Web Service/demo/src/main/java/com/example/beans/Customer.java
@@ -0,0 +1,117 @@
+package com.example.beans;
+
+public class Customer {
+	/*
+	 * CustomerID
+	 */
+	private String CustomerID;
+	
+	/*
+	 * EmailID
+	 */
+	private String EmailID;
+	/*
+	 * Fullname
+	 */
+	private String Fullname;
+	/*
+	 * Address
+	 */
+	private String Address;
+	/*
+	 * City
+	 */
+	private String City;
+	/*
+	 * State
+	 */
+	private String State;
+	/*
+	 * Zip
+	 */
+	private int Zip;
+	/*
+	 * MobNo
+	 */
+	private int MobNo;
+	/*
+	 * CreditCardNo
+	 */
+	private int CreditCardNo;
+	/*
+	 * CreditCardType
+	 */
+	private String CreditCardType;
+	/*
+	 * ExpDate
+	 */
+	private String ExpDate;
+	
+	
+	public String getCustomerID() {
+		return CustomerID;
+	}
+	public void setCustomerID(String customerID) {
+		CustomerID = customerID;
+	}
+	public String getEmailID() {
+		return EmailID;
+	}
+	public void setEmailID(String emailID) {
+		EmailID = emailID;
+	}
+	public String getFullname() {
+		return Fullname;
+	}
+	public void setFullname(String fullname) {
+		Fullname = fullname;
+	}
+	public String getAddress() {
+		return Address;
+	}
+	public void setAddress(String address) {
+		Address = address;
+	}
+	public String getCity() {
+		return City;
+	}
+	public void setCity(String city) {
+		City = city;
+	}
+	public String getState() {
+		return State;
+	}
+	public void setState(String state) {
+		State = state;
+	}
+	public int getZip() {
+		return Zip;
+	}
+	public void setZip(int zip) {
+		Zip = zip;
+	}
+	public int getMobNo() {
+		return MobNo;
+	}
+	public void setMobNo(int mobNo) {
+		MobNo = mobNo;
+	}
+	public int getCreditCardNo() {
+		return CreditCardNo;
+	}
+	public void setCreditCardNo(int creditCardNo) {
+		CreditCardNo = creditCardNo;
+	}
+	public String getCreditCardType() {
+		return CreditCardType;
+	}
+	public void setCreditCardType(String creditCardType) {
+		CreditCardType = creditCardType;
+	}
+	public String getExpDate() {
+		return ExpDate;
+	}
+	public void setExpDate(String expDate) {
+		ExpDate = expDate;
+	}
+}
