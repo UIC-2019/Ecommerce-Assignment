47e7aedcd78f465fa6f362a720fe9c211a022a8b
harshitha719 <55902125+harshitha719@users.noreply.github.com>
2019-12-04 17:36:56 -0600

    Add files via upload

---
+++ Sample Rest Web Service/demo/src/main/java/com/example/Product.java
@@ -0,0 +1,22 @@
+package com.example.beans;
+
+public class Product {
+	
+	private Integer productId;
+	
+	private String productName;
+	
+	private String productCategory;
+	
+	private String productDescription;
+	
+	private String productBrand;
+	
+	private String productManufacturer;
+	
+	private String productModel;
+	
+	
+	
+
+}
